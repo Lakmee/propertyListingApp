a82ba8610cbeb391602dde4afe43cb7a765db949
Lakmee <lakmee123@gmail.com>
2021-01-13 22:49:53 +1100

    Display property cards in the Saved Properties column

--- src/containers/Properties.js
+++ src/containers/Properties.js
@@ -4,9 +4,11 @@ import data from "../data/property-data.json";
 
 export const Properties = () => {
   const [results, setResults] = useState([]);
+  const [savedProperties, setSavedProperties] = useState([]);
 
   useEffect(() => {
     setResults(data.results);
+    setSavedProperties(data.saved);
   }, []);
 
   return (
@@ -15,6 +17,11 @@ export const Properties = () => {
         type={"resultsType"}
         properties={results}
         sectionHeading={"Results"}
+      />
+      <PropertySection
+        type={"savedPropertiesType"}
+        properties={savedProperties}
+        sectionHeading={"Saved Properties"}
       />
     </main>
   );
