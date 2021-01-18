54864fc4974c0a887ca6fbaad9c686ab4bd890fe
Lakmee <lakmee123@gmail.com>
2021-01-19 09:37:12 +1100

    Adding test for App component

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,22 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders App component", () => {
-  render(<App />);
-  //screen.debug();
+describe("App", () => {
+  test("renders App componenet with App Header, App Footer and Property Listing", () => {
+    render(<App />);
+
+    //renders App Header
+    const appHeaderHeading = screen.getByText("Property Listing");
+    expect(appHeaderHeading).toBeInTheDocument();
+
+    //renders App Footer
+    const appFooterText = screen.getByText("Application footer sample text");
+    expect(appFooterText).toBeInTheDocument();
+
+    //renders Property Listing
+    const appResultPropertyHeading = screen.getByText("Results");
+    expect(appResultPropertyHeading).toBeInTheDocument();
+    const appSavedPropertyHeading = screen.getByText("Saved Properties");
+    expect(appSavedPropertyHeading).toBeInTheDocument();
+  });
 });
