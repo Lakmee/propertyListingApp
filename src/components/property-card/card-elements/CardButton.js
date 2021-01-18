5c772a97e4b46850e5ac7244f8934c81dcc8e11d
Lakmee <lakmee123@gmail.com>
2021-01-19 00:28:00 +1100

    Making Card Buttons keyboard accessible and update styling

--- src/components/property-card/card-elements/CardButton.js
+++ src/components/property-card/card-elements/CardButton.js
@@ -3,7 +3,8 @@ import "./CardButton.scss";
 
 export const CardButton = ({ type, buttonAction, propertyId }) => {
   return (
-    <div className="card-overlay" tabIndex="0">
+    <div className="button-wrapper">
+      <div tabIndex="0"></div>
       <button onClick={() => buttonAction(propertyId)}>
         {type === "savedPropertiesType" ? "Remove Property" : "Add Property"}
       </button>
