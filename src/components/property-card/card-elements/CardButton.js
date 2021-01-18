f08fce1e62cc0f8c3a06d92fb3ad6e13a2c66a52
Lakmee <lakmee123@gmail.com>
2021-01-18 18:16:09 +1100

    Making property card button overlay tabbable

--- src/components/property-card/card-elements/CardButton.js
+++ src/components/property-card/card-elements/CardButton.js
@@ -3,7 +3,7 @@ import "./CardButton.scss";
 
 export const CardButton = ({ type, buttonAction, propertyId }) => {
   return (
-    <div className="card-overlay">
+    <div className="card-overlay" tabIndex="0">
       <button onClick={() => buttonAction(propertyId)}>
         {type === "savedPropertiesType" ? "Remove Property" : "Add Property"}
       </button>
